895fe21697c8a3b87e330fca28cf4dbf7131741b
Navnedia <87249556+Navnedia@users.noreply.github.com>
2024-05-24 04:53:27 -0500

    Initial Commit

---
+++ Medication-Tracker/Medication_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Medication_TrackerApp.swift
+//  Medication-Tracker
+//
+//  Created by Aiden Vandekerckhove on 5/24/24.
+//
+
+import SwiftUI
+
+@main
+struct Medication_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
